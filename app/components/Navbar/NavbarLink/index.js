c857243e042939c403678b135099e463af538903
Jaroslaw Zabiello <hipertracker@gmail.com>
2016-07-13 14:57:43 +0100

    removed sass form the config

---
+++ app/components/Navbar/NavbarLink/index.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line
+import React from 'react'
+import {connect, Link} from 'cerebral-view-react'
+import classNames from 'classnames'
+import {PageType, StringType} from '../../types'
+
+const NavbarLink = ({page, current, label, signals}) => {
+  const styles = classNames({
+    'uk-active': page === current
+  })
+  return (
+    <li className={styles}>
+      <Link signal={signals[page].pageOpened}>
+        {label}
+      </Link>
+    </li>
+  )
+}
+
+NavbarLink.propTypes = {
+  page: PageType,
+  current: PageType,
+  label: StringType
+}
+
+export default connect({
+  currentPage: 'app.currentPage'
+}, NavbarLink)
+
+
+
