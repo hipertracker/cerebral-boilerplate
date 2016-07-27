5b3441b4786d76969aebe263a05acc715011e0bc
Jaroslaw Zabiello <hipertracker@gmail.com>
2016-07-27 21:52:40 +0100

    modernized RecorderButton example

--- app/components/RecorderButton/index.js
+++ app/components/RecorderButton/index.js
@@ -1,60 +1,29 @@
 import React from 'react'
 import {connect} from 'cerebral-view-react'
-
-// source: http://www.cerebraljs.com/documentation/cerebral-module-recorder
+import styles from './style.css'
 
 export default connect({
-  recorder: 'recorder'
-}, props => {
-  const style = {
-    border: '1px solid black',
-    borderRadius: '2px',
-    height: '25px',
-    backgroundColor: '#EAEAEA',
-    padding: '5px',
-    lineHeight: '15px',
-    boxSizing: 'border-box'
-  }
-  const signals = props.signals
-
-  if (props.recorder.isPlaying) {
-    return React.createElement('button', {
-      style,
-      onClick: () => {
-        signals.recorder.paused({}, {
-          isRecorded: true
-        })
-      }
-    }, 'Pause playback')
-  }
-  if (props.recorder.isPaused) {
-    return React.createElement('button', {
-      style,
-      onClick: () => {
-        signals.recorder.resumed()
-      }
-    }, 'Play')
-  }
-  if (props.recorder.isRecording) {
-    return React.createElement('button', {
-      style,
-      onClick: () => {
-        signals.recorder.stopped()
-      }
-    }, 'Stop recording')
-  }
-  if (props.recorder.hasRecorded) {
-    return React.createElement('button', {
-      style,
-      onClick: () => {
-        signals.recorder.played()
-      }
-    }, 'Play')
-  }
-  return React.createElement('button', {
-    style,
-    onClick: () => {
-      signals.recorder.recorded()
+    recorder: 'recorder'
+  }, {
+    clicked: 'recorder'
+  },
+  function RecorderButton({recorder, clicked}) {
+    let action
+    if (recorder.isPlaing) {
+      action = {name: 'paused', label: 'Pause playback'}
+    } else if (recorder.isPaused) {
+      action = {name: 'resumed', label: 'Play'}
+    } else if (recorder.isRecording) {
+      action = {name: 'stopped', label: 'Stop recording'}
+    } else if (recorder.hasRecorded) {
+      action = {name: 'played', label: 'Play'}
+    } else {
+      action = {name: 'recorded', label: 'Record'}
     }
-  }, 'Record')
-})
+    return (
+      <button className={styles.recorder} onClick={() => clicked[action.name]()}>
+        {action.label}
+      </button>
+    )
+  }
+)
