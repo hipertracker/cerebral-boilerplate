98989ce0a0e4c8b6d1b4676dfd4342cefa9d2ffd
Jaroslaw Zabiello <hipertracker@gmail.com>
2016-07-27 22:00:55 +0100

    cleaned imports in unit test

--- test/components/App.test.js
+++ test/components/App.test.js
@@ -1,13 +1,11 @@
-// eslint-disable-next-line
 import React from 'react'
 import {shallow} from 'enzyme'
-import App from '../../app/components/App'
-import Navbar from '../../app/components/App/Navbar'
+import App from 'components/App'
+import Navbar from 'components/App/Navbar'
 
 describe('<App/>', () => {
   it('renders <Navbar/>', () => {
     const wrapper = shallow(<App/>)
     expect(wrapper.find(Navbar)).to.have.length.of(1)
   })
-
 })
