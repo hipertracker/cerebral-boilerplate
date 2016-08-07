17b3b649fae48acf96a43aa6f4da693088dbd6ce
Jaroslaw Zabiello <hipertracker@gmail.com>
2016-08-07 14:27:22 +0100

    remove why-did-you-update

--- app/main.js
+++ app/main.js
@@ -4,11 +4,4 @@ import {Container} from 'cerebral-view-react'
 import controller from 'lib/controller'
 import App from 'components/App'
 
-
-if (process.env.NODE_ENV !== 'production') {
-  const {whyDidYouUpdate} = require('why-did-you-update')
-
-  whyDidYouUpdate(React)
-}
-
 render(<Container controller={controller}><App/></Container>, document.querySelector('#app'))
